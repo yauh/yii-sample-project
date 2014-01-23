46a0c9b264ac837ef9af93d2ea5ea509b10a9473
Stephan Hochhaus <stephan@yauh.de>
2014-01-23 09:15:57 +0100

    Initial commit
    This is the first commit for this project.

---
+++ src/protected/tests/unit/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * A dummy test that will always return true
+ * @author stephan
+ * @version 1.0.0
+ * @todo write a more sensible test
+ */
+class DummyTest extends CTestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic='/var/www/yii/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
